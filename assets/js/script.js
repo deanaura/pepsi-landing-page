52d0bb0bc47abcc8b2dea33b4d3736c74e17146e
deanaura <deanaura58@gmail.com>
2024-08-24 14:17:48 +0700

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+function imgSlider(anything){
+    document.querySelector('.pepsi').src = anything;
+}
+
+function changeBgColor(color){
+    const bgColor = document.querySelector('.main');
+    bgColor.style.background = color;
+}
